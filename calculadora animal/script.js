da9310266b10ef67170337607912104493b5fd83
Vitor42z <78519292+Vitor42z@users.noreply.github.com>
2021-09-21 11:33:44 -0300

    atividades

---
+++ calculadora animal/script.js
@@ -0,0 +1,5 @@
+function calcularIdadePet(){
+    var idade = Number(prompt("insira a idade do cachorro")) * 7;
+    document.write("a idade do seu cachorro é: " + idade + "<br>");
+}
+calcularIdadePet();
